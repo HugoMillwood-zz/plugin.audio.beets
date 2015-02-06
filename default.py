300e5dce800fb0e9aad5393b1547f0852dd669d5
Pär Strindevall <par@strindevall.com>
2015-02-06 18:06:56 +0100

    Added settings and stuff

--- default.py
+++ default.py
@@ -8,18 +8,25 @@ import urllib2
 import urllib
 import itertools, collections
 
-# SETTINGS
-ip_address = "192.168.1.5"
-port = "8337"
-
 # ADDON DATA
 beets = xbmcaddon.Addon('plugin.audio.beets')
 addon_handle = int(sys.argv[1])
 xbmcplugin.setContent(addon_handle, 'audio')
 
+# SETTINGS
+
+ip_address = beets.getSetting('ip')
+port = beets.getSetting('port')
+
+# ??
+
+args = urlparse.parse_qs(sys.argv[2][1:])
+
 # FUNCTIONS
 def consume(iterator, n):
 	collections.deque(itertools.islice(iterator, n))
+	
+#def presentData(dataList):
 
 def getArtists():
 	print("DEBUG: Fetching artist data.")
@@ -31,19 +38,15 @@ def getArtists():
 		for number in iterator:
 			artist = result[number].encode("UTF-8") #THIS IS HILAAAAR
 			#artist = result[number].get().encode("UTF-8")
-			print(artist)
-			print("iterates")
 			#id = result[number].get('id')
 			#url = 'http://'+ip_address+':'+str(port)+'/item/'+str(id)+'/file'
 			url = "doubleyoudoubleyoudoubleyoudotwhatevermandotcom"
 			li = xbmcgui.ListItem(artist, iconImage='DefaultAudio.png')
-			li.setProperty('fanart_image', beets.getAddonInfo('fanart'))
+			#li.setProperty('fanart_image', beets.getAddonInfo('fanart'))
 			if (artist != previousArtist):
 				xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 				previousArtist = artist
+		xbmcplugin.endOfDirectory(addon_handle)
 
 # MAIN
 getArtists()
-
-xbmcplugin.endOfDirectory(addon_handle) #where should this be?
-
